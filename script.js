7b22e74f54648ea069c56bcb4c752a48511f3a9b
Martin Staadecker <machstg@gmail.com>
2019-11-19 13:50:22 -0500

    Extract category names as constants
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const binsImage = document.getElementById("bins");
 
+const COFFEE = "coffee";
+const PAPER = "paper";
+const CONTAINER = "container";
+const GARBAGE = "garbage";
+
 binsImage.addEventListener("load", init);
 
 function init() {
     const binContent = binsImage.contentDocument;
-    binContent.getElementById("Coffee_Slot").addEventListener("click", () =>
-        onCategoryClick("coffee")
-    );
 
-    binContent.getElementById("Garbage_Slot").addEventListener("click", () =>
-        onCategoryClick("garbage")
-    );
-
-    binContent.getElementById("Paper_Slot").addEventListener("click", () =>
-        onCategoryClick("paper")
-    );
-
-    binContent.getElementById("Container_Slot").addEventListener("click", () =>
-        onCategoryClick("container")
-    );
+    binContent.getElementById("Coffee_Slot").addEventListener("click", () => onCategoryClick(COFFEE));
+    binContent.getElementById("Garbage_Slot").addEventListener("click", () => onCategoryClick(GARBAGE));
+    binContent.getElementById("Paper_Slot").addEventListener("click", () => onCategoryClick(PAPER));
+    binContent.getElementById("Container_Slot").addEventListener("click", () => onCategoryClick(CONTAINER));
 }
 
 function onCategoryClick(categoryName) {
