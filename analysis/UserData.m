acf149c14d5b1edf40bf27dfe42849c78ad8e05f
Martin Staadecker <machstg@gmail.com>
2019-11-26 17:14:17 -0500

    Add 4th test case
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- analysis/UserData.m
+++ analysis/UserData.m
@@ -1,9 +1,21 @@
 classdef UserData
     properties
         timestamp
-        category
+        bin_file
         data
         delay
+    end
+    
+    methods
+        function correct_data(obj)
+            if obj.bin_file == "myhal_bin.svg"
+                for i = 1:length(obj.data)
+                    if strcmp(obj.data(i,3), "container")
+                       
+                    end
+                end
+            end
+        end
     end
 end
 
